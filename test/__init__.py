ff4b575196f1bacac28ebdc79465ad61f6fa244b
qcuincy <quincy.sproul@gmail.com>
2023-05-01 23:33:54 +0100

    fix __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -2,5 +2,4 @@ from .test_numerical_methods import *
 from .test_bvp_methods import *
 from .test_ivp_methods import *
 from .test_pde_utils import *
-from .examples import *
 from .context import *
